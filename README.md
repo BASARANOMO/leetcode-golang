413bec821ada297297cb2f9fb2c802ad163d857d
BASARANOMO <basaranomo@outlook.com>
2023-04-22 21:53:36 +0800

    add solution to question to question 1027

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Provide all my solutions and explanations for LeetCode coding problems.
 
 ## Categories
 
+### Dynamic programming
+
+[1027. Longest Arithmetic Subsequence](https://github.com/BASARANOMO/leetcode-golang/tree/main/solutions/Medium/1027.%20Longest%20Arithmetic%20Subsequence)
---
+++ solutions/Medium/1027. Longest Arithmetic Subsequence/solution.go
@@ -0,0 +1,31 @@
+func longestArithSeqLength(nums []int) int {
+    minv, maxv := nums[0], nums[0]
+    for _, num := range nums[1:] {
+        if num < minv {
+            minv = num
+        } else if num > maxv {
+            maxv = num
+        }
+    }
+    diff := maxv - minv
+    ans := 1
+    for d := -diff; d <= diff; d++ {
+        f := make([]int, maxv + 1)
+        for _, num := range nums {
+            prev := num - d
+            if prev >= minv && prev <= maxv {
+                f[num] = max(f[num], f[prev] + 1)
+                ans = max(ans, f[num])
+            }
+            f[num] = max(f[num], 1)
+        }
+    }
+    return ans
+}
+
+func max(a, b int) int {
+    if b > a {
+        return b
+    }
+    return a
+}
